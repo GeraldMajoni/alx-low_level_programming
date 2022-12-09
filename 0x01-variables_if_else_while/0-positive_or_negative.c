3c646742f45394f11023be5000a213f4bb51b33e
GeraldMajoni <majonigerald@gmail.com>
2022-12-09 05:52:33 -0800

    Script to test Random numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry into program
+ * printing results of condition
+ * Return: (0) (Successful
+ */
+if (n > int 0)
+{
+	printf("%d is positive\n", n);
+}
+else if (n == int 0)
+{
+	printf("%d is zero\n", n);
+}
+else
+{
+	printf("%d is negative\n", n);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+0. Script to test conditions
