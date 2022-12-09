a43468cfad84e545fc88bd34abc79147ac399a9b
GeraldMajoni <majonigerald@gmail.com>
2022-12-09 09:35:57 -0800

    Program to print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry into program
+ * Return: (0) (success)
+ * printing ASCII
+ * exit
+ */
+int main(void)
+{
+	for (int i = 97; i < 123)
+	{
+		putchar(i);
+	}
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,3 @@
 0. Script to test conditions
 1. Script to print last digit and print conditions
+2. Program to print letters of the alphabet
