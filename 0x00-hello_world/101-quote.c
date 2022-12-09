8029766cadbb909b32b1242227b2ebfd4106aed3
GeraldMajoni <majonigerald@gmail.com>
2022-12-08 16:04:31 -0800

    Script to write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stidio.h>
 #include <unistd.h>
 /**
  * main - entry int program
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
 
