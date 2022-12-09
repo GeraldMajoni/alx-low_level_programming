f3c4ed1ed01434a51c5be942e0b150794793eb7f
GeraldMajoni <majonigerald@gmail.com>
2022-12-09 06:18:08 -0800

    Script for if else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,17 +2,21 @@
 /**
  * main - Entry into program
  * printing results of condition
- * Return: (0) (Successful
+ * Return: (0) (Successful)
+ * x: test
  */
-if (n > int 0)
+int main(void)
 {
-	printf("%d is positive\n", n);
-}
-else if (n == int 0)
-{
-	printf("%d is zero\n", n);
-}
-else
-{
-	printf("%d is negative\n", n);
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
 }
