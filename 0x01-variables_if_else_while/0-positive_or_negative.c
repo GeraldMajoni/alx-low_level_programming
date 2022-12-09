1830c71c546842410b8b7d4d2ebb2e5fe10cf7c5
GeraldMajoni <majonigerald@gmail.com>
2022-12-09 06:32:15 -0800

    Script for is and else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,18 @@
-#include <stdio.h>
+#include <stdlib.h>
+
+#include <time.h>
+
 /**
- * main - Entry into program
- * printing results of condition
- * Return: (0) (Successful)
- * x: test
+ * main - Entry to program
+ * Return: (0) (successful)
  */
+
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
