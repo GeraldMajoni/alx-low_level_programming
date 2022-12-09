665822a2ead06b33703f115af405cfaff86fe519
GeraldMajoni <majonigerald@gmail.com>
2022-12-09 10:51:11 -0800

    Program to print upper and lower alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry mode
+ * Loops to print alphabet
+ * Return: (0) (success)
+ */
+int main(void)
+{
+	int i = 97, j = 65;
+
+	while (i < 123)
+	{
+		putchar(i);
+	}
+	while (j < 91)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,3 +1,4 @@
 0. Script to test conditions
 1. Script to print last digit and print conditions
 2. Program to print letters of the alphabet
+3. Program to print upper and lower case alphabet
