57da0d5f07cff1d17a3fb79136f9e20712775f9e
GeraldMajoni <majonigerald@gmail.com>
2022-12-13 08:24:52 -0800

    Program to check case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _islower - Entry into program
+ * @c: character
+ * Return: (1) (successful)
+ */
+int _islower(int c)
+{
+	int r;
+	char c = 'a';
+
+	r = _islower(int c);
+
+	while (c <= 'z')
+	{
+		if (_putchar(r + '0') == _putchar(c))
+		{
+			return (1);
+			_putchar('\n');
+		}
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0. Program to print _putchar
 1. Program to print alphabet
 2. Print alphabet 10 times
+3. Program to check casee
