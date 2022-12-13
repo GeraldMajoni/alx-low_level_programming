6010aa084ea9ecf30b12d5c43657ec38507115dc
GeraldMajoni <majonigerald@gmail.com>
2022-12-13 12:50:40 -0800

    Program to print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_last_digit - Compute the last digit
@@ -12,6 +11,7 @@ int print_last_digit(int n)
 	int m;
 
 	m = n % 10;
-	printf("%d", m);
+	m = m + 48;
+	_putchar(m);
 	return (m);
 }
