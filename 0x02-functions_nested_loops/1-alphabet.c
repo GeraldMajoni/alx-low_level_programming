ab639d238fdf49bc922d888163f1aa7c64fc1c4c
GeraldMajoni <majonigerald@gmail.com>
2022-12-13 04:08:32 -0800

    Program to print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry into Program
+ *
+ * Return: (0) (success)
+ */
+
+int main(void)
+{
+	_putchar(print_alphabet());
+	_putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 0. Program to print _putchar
+1. Program to print alphabet
