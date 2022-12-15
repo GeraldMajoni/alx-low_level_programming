e198afe4b3834c72cc76a965e61bed9a7e145566
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 08:07:24 -0800

    Program to check upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry into program to find uppercase
+ * print results
+ * @c: integer input
+ * Return: (1) (success)
+ */
+
+int _isupper(int c);
+{
+	int c;
+
+	if (c >= 'A' && c <= 'Z')
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ')
+		return (1);
+	}
+	else
+	{
+		_putchar(c);
+		_putchar(':');
+		_putchar(' ');
+		return (0);
+	}
+}
