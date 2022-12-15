095d3972c8118ad2c8def0d88e5a28fec847e234
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 13:30:33 -0800

    Program to print a line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,22 +4,16 @@
  * print_line - entry to program
  * print lines
  * @n: integer parameter
- * Return: 0 success
+ * Return: nil success
  */
 
 void print_line(int n)
 {
 	int i;
 
-	if (n == 0 || n < 0)
-		_putchar('\n');
-	else
-	{
-		for (i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		{
 			_putchar('_');
 		}
 	_putchar('\n');
-
-	return (0);
 }
