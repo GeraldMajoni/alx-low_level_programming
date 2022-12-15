6dd379da122143b8957644562374565e0c243ec7
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 14:33:20 -0800

    Program to draw a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,13 +11,16 @@ void print_square(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 0; j < size; j++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			if (i == size - 1)
+				continue;
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 	_putchar('\n');
 }
