26be29f7a4c9e754d15ed52b6da1b7ac3cfd9969
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 09:54:09 -0800

    Program to print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Entry into program
+ *
+ * Return: nil success
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0. Program to check upper case
 1. Program to check if digit 
 2. Program to multiply two numbers
+3. Program to print 0-9
