ab0bd064a208da88a9903b8570c006ca06ff4656
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 13:06:07 -0800

    Program to print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,17 +8,16 @@
 
 void more_numbers(void)
 {
-	int num, count;
+	int i, j;
 
-	for (count = 0; count <= 9; count++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (j = 0; j <= 14; j++)
 		{
-			if (num >= 10)
-			{
+			if (j >= 10)
 				_putchar('1');
-			_putchar((num % 10) + '0');
-			}
+			_putchar(j % 10 + '0');
+			
 		}
 		_putchar('\n');
 	}
