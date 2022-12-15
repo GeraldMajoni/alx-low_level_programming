58121a60b4cf308566c91c72a9d55e63286540c9
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 10:15:38 -0800

    Program to print excluding 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry into program
+ * print numbers except 2 and 4
+ * Return: nil
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i <= 57)
+	{
+		if (i == 50 || i == 52)
+		{
+			i++;
+			continue;
+		}
+		else
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 1. Program to check if digit 
 2. Program to multiply two numbers
 3. Program to print 0-9
+4. Program to print numbers excluding 2 and 4
