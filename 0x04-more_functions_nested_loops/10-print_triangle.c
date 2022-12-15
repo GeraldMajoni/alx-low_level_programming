a040a949f1a0ade4f36358fce0dc3784594d5dff
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 15:45:51 -0800

    Program to print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -15,7 +15,7 @@ void print_triangle(int size)
 		for (h = 1; h <= size; h++)
 		{
 			for (i = size - h; i > 0; i--)
-				_putchar('#');
+				_putchar(' ');
 
 			for (i = 0; i < h; i++)
 				_putchar('#');
