b613aa0be443d6612fa7ab1477c60814bb1da93f
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 15:27:36 -0800

    Program for Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,20 +9,26 @@
 int main(void)
 {
 	int i;
-	char a[] = "Fizz";
-	char b[] = "Buzz";
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if ((i % 3) == 0 && (i % 5) == 0)
 			printf("FizzBuzz");
-		if (i % 3 == 0)
-			printf("%s", a);
+
+		else if (i % 3 == 0)
+			printf("Fizz");
+
 		else if (i % 5 == 0)
-			printf("%s", b);
+			printf("Buzz");
+
 		else
 			printf("%d", i);
+
+		if (i == 100)
+			continue;
+		printf(" ");
 	}
+
 	printf("\n");
 
 	return (0);
