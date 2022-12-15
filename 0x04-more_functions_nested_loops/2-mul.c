5e11c6ee24f762cc8b4d6a1b3612546337c95222
GeraldMajoni <majonigerald@gmail.com>
2022-12-15 09:19:20 -0800

    Program to multiply a and b

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry to program
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 success
+ */
+
+int mul(int a, int b)
+{
+	mul(a, b);
+	return (0);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 0. Program to check upper case
 1. Program to check if digit 
+2. Program to multiply two numbers
