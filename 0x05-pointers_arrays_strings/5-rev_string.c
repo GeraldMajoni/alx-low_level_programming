620bc83e74e212e58a5fe2e21b0f126b9f7abfd5
GeraldMajoni <majonigerald@gmail.com>
2022-12-19 11:35:22 -0800

    Program to reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,38 +1,22 @@
 #include "main.h"
 
 /**
- * rev_string - printing string in reverse
- * _strlen - count string length
+ * rev_string - prints in reverse
  * @s: string
  * Return: Nil
  */
 
-int _strlen(char *s)
-{
-	int i, len;
-
-	len = 0;
-
-	for (i = 0; s[i] != '\0'; ++i)
-	{
-		len++;
-	}
-	return (len);
-}
 void rev_string(char *s)
 {
-	int strsize = _strlen(s);
-	int i;
+	int l, i;
+	char ch;
 
-	char c;
-
-	i = 0;
-
-	while (i < (strsize / 2))
+	for (l = 0; s[1] != '\0'; i++)
+		;
+	for (i = 0; i < l / 2; ++i)
 	{
-		c = *(s + i);
-		*(s + i) = *(s + strsize - 1 - i);
-		*(s + strsize - 1 - i) = c;
-		i++;
+		ch = s[i];
+		s[i] = s[l - 1 - i];
+		s[l - 1 - i] = ch;
 	}
 }
