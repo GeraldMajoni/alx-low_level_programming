11bfaae4d131d08019592beeac3f127bbfd3f71b
GeraldMajoni <majonigerald@gmail.com>
2022-12-21 20:16:14 -0800

    Program to concatenate string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- * _strcat - Entry to program to concatenate
- * @dest: pointer to string
- * @src: pointer to string
+ *_strcat - Entry to program to concatenate
+ *@dest: pointer to string
+ *@src: pointer to string
  * Return: dest pointer
  */
 
-char *_strcat(char *dest, char *src);
+char *_strcat(char *dest, char *src)
 {
-	int index = 0, dest_length = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 	{
-		dest_length++;
+		dest_len++;
 	}
 
 	for (index = 0 ; src[index]; index++)
 	{
-		dest[dest_length++] = src[index];
+		dest[dest_len++] = src[index];
 	}
 	return (dest);
 }
